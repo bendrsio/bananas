d5c16ff22dc98dec4318d9510fd19d481715ac8f
bendrsio <benjy.desprets@gmail.com>
2025-07-28 20:49:40 -0400

    add: editor.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,17 @@
 import { createRoot } from "react-dom/client";
+import Editor from "./components/editor";
 
 const root = createRoot(document.body);
 root.render(
-  <div>
+  <div
+    style={{
+      width: "100%",
+      height: "100%",
+      display: "flex",
+      flexDirection: "column",
+    }}
+  >
     <h1>Text Editor</h1>
-    <textarea />
+    <Editor />
   </div>
 );
---
+++ src/components/editor.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const Editor: React.FC = () => {
+  const [text, setText] = useState<string>("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <textarea
+      style={{
+        width: "100%",
+        height: "100%",
+        border: "none",
+        padding: "20px",
+        fontSize: "16px",
+        fontFamily: "monospace",
+        resize: "none",
+        boxSizing: "border-box",
+        outline: "none",
+        // backgroundColor: "#282c34",
+        // color: "#abb2bf",
+      }}
+      value={text}
+      onChange={handleChange}
+      placeholder="Start typing here..."
+    />
+  );
+};
+
+export default Editor;
